42713295e238dcc4f6a353fb0f93fe4249312d9d
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-03-22 12:33:28 -0400

    moved App.js

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -2,6 +2,6 @@ import React, {Component} from 'react';
 
 export default class App extends Component {
     render () {
-        return <p>This is my new react app and it's nice</p>
+        return <p>This is my new REACT APP and it's nice</p>
     }
 }
