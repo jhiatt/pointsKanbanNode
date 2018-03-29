d3988f32f275570703fe373d8b859eeda434e1ea
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-03-29 11:51:18 -0400

    test data flows into API

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
 
   //could pass dev or production as variable of function for different db connections.
   getDbConnectionString: function () {
-    return 'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_USERNAME + '@ds123129.mlab.com:23129/points-kanban';
+    return 'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@ds123129.mlab.com:23129/points-kanban';
   }
 
 }
--- controllers/apiController.js
+++ controllers/apiController.js
@@ -18,4 +18,38 @@ module.exports = function(app) {
     });
 
   });
+
+  app.get('/api/test/:id', function(req, res) {
+
+    Tests.findById({ _id: req.params.id }, function(err, test) {
+      if (err) throw err;
+
+      res.send(test);
+    });
+  });
+
+  app.post('/api/test', function(req, res) {
+    
+    //check if this is an UPDATE (id is created by database so you wouldn't add it to the req)
+    if (req.body.id) {
+      Tests.findByIdAndUpdate(req.body.id, {
+        test: req.body.test
+      }, function(err, test) {
+          if (err) throw err;
+
+          res.send('success');
+      });
+    } else {
+
+      var newTest = Tests({
+        username: 'jhiatt',
+        test: req.body.test
+      });
+      newTest.save(function(err) {
+        if (err) throw err;
+        res.send('Success');
+      })
+    }
+
+  });
 }
